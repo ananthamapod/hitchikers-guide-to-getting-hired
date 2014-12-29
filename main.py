bbd2c8eaf2f9d35a89be378630d1c308d196fb5b
Ananth Rao <ananthamapod@gmail.com>
2014-12-29 10:48:48 -0500

    init: take 5

--- main.py
+++ main.py
@@ -16,27 +16,9 @@ def user():
 def show_user_profile(name):
 	return 'User %s' % name
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-	return 'Post %d' % post_id
-
-@app.route('/projects/')
-def projects():
-	return 'The projects page'
-
-@app.route('/about')
-def about():
-	return 'The about page'
-
-
 @app.route('/S1/<name>')
 def state1(name):
 	return render_template('s1.html', name = name)
-
-@app.route('/C1/')
-def case1_1():
-	return
-
 
 if __name__ == '__main__':
 	app.run()
